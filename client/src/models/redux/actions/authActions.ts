8fd1a0e96b8011b38c47d6cce1850042bf1cc822
Glebario <works.suhovoy@gmail.com>
2020-09-19 11:51:57 +0300

    The Clean Architecture (test)

--- client/src/models/redux/actions/authActions.ts
+++
@@ -1,5 +0,0 @@
-export const authActions = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER' as const,
-    GET_ERRORS_FROM_SERVER: 'GET_ERRORS_FROM_SERVER' as const,
-    UPDATE_AUTH_STORE: 'UPDATE_AUTH_STORE' as const,
-}
--- client/src/models/redux/actions/sharedActions.ts
+++
@@ -1,4 +0,0 @@
-export const sharedActions = {
-    INCLUDE_LOADING_LOGO: 'INCLUDE_LOADING_LOGO' as const,
-    DEFAULT_STATE: 'DEFAULT_STATE' as const
-}
